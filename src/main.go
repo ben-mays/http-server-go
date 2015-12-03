bb7e45f1231132783c57d4680ae60a42ecd59ded
ben-mays <ben@eastside.io>
2015-12-03 14:13:26 -0500

    Added a default file path for /, to load an index.html page

--- src/main.go
+++ src/main.go
@@ -15,6 +15,9 @@ func main() {
   sendFileHandler = func(request http.HttpRequest) (http.HttpResponse, error) {
     // get the file
     file := request.URI
+    if file == "/" {
+      file = "/index.html"
+    }
     fmt.Printf("Sending file %s%s\n", rootDir, file)
     f, ferr := os.Open(fmt.Sprintf("%s%s", rootDir, file))
     defer f.Close()
@@ -36,6 +39,8 @@ func main() {
       // TODO: Send chunk encoded responses
       body = append(body, buffer[:n]...) // append buffer[0-n) to the body buffer.
     }
+
+    // TODO: Figure out proper content-length. CURL is reporting excess bytes in the body, should we count the CLRF bytes here?
     headers := []string{"Connection: close", fmt.Sprintf("Content-Length: %d", len(body)+2)}
     return http.NewHttpResponse(http.Protocol11, http.Status200, headers, body), nil
   }
